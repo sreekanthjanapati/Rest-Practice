b9b6611280abd5fbd802d7426c26b1b4342f25fd
sreekanthjanapati <sreekanth@test.com>
2020-04-11 12:58:40 +0530

    Hello controller

--- src/main/java/com/restapi/controllers/HelloController.java
+++ src/main/java/com/restapi/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.restapi.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1/users")
 public class HelloController {
 	
-	
+	@GetMapping
 	public String getUsersData() {
 		
 		return "kanth";
