4e0ece550efa166e57a4c0fc4ff497d4005b9a7c
sreekanthjanapati <sreekanth@test.com>
2020-04-12 02:30:48 +0530

    removed unnessery imports

--- src/main/java/com/restapi/controllers/HelloController.java
+++ src/main/java/com/restapi/controllers/HelloController.java
@@ -1,14 +1,11 @@
 package com.restapi.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.restapi.model.User;
 import com.restapi.model.UsersInfo;
 import com.restapi.service.HelloService;
 
--- src/main/java/com/restapi/controllers/UserController.java
+++ src/main/java/com/restapi/controllers/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.restapi.exceptions.UserServiceExcepiton;
 import com.restapi.model.User;
 import com.restapi.model.UsersInfo;
 import com.restapi.service.HelloService;
