e077bbe81ce8eac4ee08996824b34293c492b167
sreekanthjanapati <sreekanth@test.com>
2020-04-11 20:19:09 +0530

    Comitting the data

--- src/main/java/com/restapi/controllers/UserController.java
+++ src/main/java/com/restapi/controllers/UserController.java
@@ -3,8 +3,10 @@ package com.restapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +15,22 @@ import com.restapi.model.UsersInfo;
 import com.restapi.service.HelloService;
 
 @RestController
-@RequestMapping("v1/hello")
-public class HelloController {
+@RequestMapping("v1/users")
+public class UserController {
 
 	@Autowired
 	HelloService service;
 
 	@GetMapping
-	public List<User> getUsersData(@ModelAttribute UsersInfo info ) {
+	public List<User> getUsersData(@ModelAttribute UsersInfo info) {
 
-		 System.out.println("name in the request param is:: "+info);
+		System.out.println("name in the request param is:: " + info);
 		return service.getUsersInfo();
 	}
-
+	
+	public ResponseEntity<User> insertUsersData(@RequestBody User user) {
+		System.out.println("Got the users data from client :: "+user);
+		return ResponseEntity.ok().body(user);
+		
+	}
 }
--- src/main/java/com/restapi/model/User.java
+++ src/main/java/com/restapi/model/User.java
@@ -1,32 +1,15 @@
 package com.restapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-
+@Data
+@AllArgsConstructor
 public class User {
 
 	private int id;
 	private String name;
 
-	public User(int id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
+	
 
 }
